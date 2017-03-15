e705248c892139757ceb651f5ad814931e65b064
tahabakk <tahabakk2@gmail.com>
2017-03-15 16:37:11 +0100

    Finalizado comprobado todo ya

--- src/Main.java
+++ src/Main.java
@@ -12,19 +12,20 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String rutaFacktbook = "/home/x3727349s/Baixades/Factbook.xml";
-        String rutaMondial = "/home/x3727349s/Baixades/Mondial.xml";
-        String bbdd = "tahabakk";
-        String username = "admin";
-        String pass = "admin";
-        String consulta = "";
-        String ip ="localhost";
+        String rutaFacktbook = "/home/x3727349s/Baixades/Factbook.xml";//cambiar por la ruta absoluta
+        String rutaMondial = "/home/x3727349s/Baixades/mondial.xml";//cambiar por la ruta absoluta
+        String bbdd = "tahabakk";//Nombre de la bbdd
+        String username = "admin";//usuario de baseX
+        String pass = "admin";//Contrasenya de baseX
+        String consulta = "";//Consulta se añade en el switch dependiendo de la opcion seleccionada
+        String ip ="localhost";//ip del server/baseX
 
         Scanner sc = new Scanner (System.in);
 
         int numOpcion = 0;
         do {
-
+            System.out.println("");
+            System.out.println("--------------------------------------------------------------------------");
             System.out.println("1.Quins països hi ha en el fitxer «factbook.xml»? ");
             System.out.println("2.Quants països hi ha?");
             System.out.println("3.Quina és la informació sobre Alemanya ?");
@@ -32,42 +33,51 @@ public class Main {
             System.out.println("5.Quines són les ciutats de Perú que recull aquest fitxer?");
             System.out.println("6.Quanta gent viu a Shanghai?");
             System.out.println("7.Quin és el codi de matricula de cotxe de Xipre?");
+            System.out.println("--------------------------------------------------------------------------");
+            System.out.println("");
 
             numOpcion = sc.nextInt();
 
             switch (numOpcion) {
                 case 1:
                     consulta = "//country";//pasamos la consulta a realizar
+                    System.out.println("Els països que conté son els següents");
                     consulta(username, pass, rutaFacktbook, bbdd, consulta, ip);//llamamos el metodo pasandole los datos
                     break; // optional
 
                 case 2:
                     consulta = "count(//country)";
+                    System.out.println("La quantitat de països que hi ha son: ");
                     consulta(username, pass, rutaMondial, bbdd, consulta, ip);
                     break; // optional
 
                 case 3:
-                    consulta = "factbook/record[country=\"Germany\"]/introduction/background";
-                    consulta(username, pass, rutaMondial, bbdd, consulta, ip);
+                    consulta = "//record[country=\"Germany\"]/introduction/background";
+                    System.out.println("La indormació sobre alemanya és:");
+                    consulta(username, pass, rutaFacktbook, bbdd, consulta, ip);
                     break; // optional
 
                 case 4:
-                    consulta = "factbook/record[country=\"Uganda\"]/people/population";
-                    consulta(username, pass, rutaMondial, bbdd, consulta, ip);
+                    consulta = "//record[country=\"Uganda\"]/people/population";
+                    System.out.println("La quantitat de població que hi ha en Uganda és: ");
+                    consulta(username, pass, rutaFacktbook, bbdd, consulta, ip);
                     break; // optional
 
                 case 5:
-                    consulta = "mondial/country[name=\"Peru\"]/province/city/name";
+                    consulta = "//country[name=\"Peru\"]/province/city/name";
+                    System.out.println("Les ciutats de peru son: ");
                     consulta(username, pass, rutaMondial, bbdd, consulta, ip);
                     break; // optional
 
                 case 6:
-                    consulta = "mondial/country[name=\"China\"]/province/city[name=\"Shanghai\"]/population";
+                    consulta = "//country[name=\"China\"]/province/city[name=\"Shanghai\"]/population";
+                    System.out.println("La quantitat de població és de: ");
                     consulta(username, pass, rutaMondial, bbdd, consulta, ip);
                     break; // optional
 
                 case 7:
-                    consulta = "mondial/country[name=\"Cyprus\"]/@car_code";
+                    consulta = "//country[name=\"Cyprus\"]/@car_code";
+                    System.out.println("La matricula es: ");
                     consulta(username, pass, rutaMondial, bbdd, consulta, ip);
                     break; // optional
 
@@ -75,43 +85,30 @@ public class Main {
                     // Statements
             }
 
-        }while (numOpcion!=0);
+        }while (numOpcion!=0);//si es 0 sale del blucle y finaliza
 
 
-
+        System.out.println("Programa finalizado ^^");
 
 
     }
 
 
-
+    //Funcio que ejecuta la consulta al fitxero
     public static void consulta(String username, String pass, String ruta, String bbdd, String consulta, String ip){
 
         try (ClientSession session = new ClientSession(ip, 1984, username, pass)) {//IP, PUERTO, USERNAME, PASSWORD
-            session.execute(new CreateDB(bbdd, ruta));//BD que se creara, y ruta del fitxero
+            session.execute(new CreateDB(bbdd, ruta));//BBDD que se creara, y ruta del fitxero
             try (ClientQuery query = session.query(consulta)) {//consulta
                 System.out.println(query.execute());//imprimir resultado
             }
             session.close();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
-    /*public void consulta(String username, String pass, String ruta){
-
-        try (ClientSession session = new ClientSession("localhost", 1984, "admin", "admin")) {
-            session.execute(new CreateDB("TahaBakk", "/home/x3727349s/Baixades/Factbook.xml"));//BD que se creara, y ruta del fitxero
-            try (ClientQuery query = session.query("//country")) {//consulta
-                System.out.println(query.execute());//imprimir resultado
-            }
-            session.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }*/
 
 
 
